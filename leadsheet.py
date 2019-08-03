286bfe43408e531660cb2678b182477589128655
André Hedesand <an7821he-s@student.lu.se>
2019-08-04 01:27:39 +0200

    Lead sheets now have a basic headers and titlepage

--- leadsheet.py
+++ leadsheet.py
@@ -3,19 +3,74 @@ from fpdf import FPDF
 class LeadSheet(FPDF):
 
     # Member functions
-    def __init__( self, margin_left=10, margin_top=10, margin_right=10,
-                        barline_width=2 ):
+    def __init__( self, title, artist, 
+                    margin_left=10, margin_top=10, margin_right=10,
+                    barline_width=2 ):
         """ Sets initial values, making generation more managable. """
         super(LeadSheet, self).__init__('P', 'mm', 'A4')
-        
-        self.columns = 4
-        self.rows = 8
-        # adjust margin_right because negative margin is stupid.
+        # Meta data
+        self.set_title(title)
+        self.artist = artist
+
+        # Fonts
+        self.chord_font = ('Arial', '', 30)
+        self.header_font = ('Arial', 'I', 12)
+        self.font_stack = []
+        self._font_stack_push(self.chord_font)
+
+        # Size and dimensions
         self.set_margins(margin_left, margin_top, -margin_right) 
-        self.set_font('Arial', '', 30)
         self.useable_width = round(self.fw) - margin_left - margin_right
         self.barline_width = 2
         self.staff_height = 30
+
+    def _font_stack_push(self, font_tuple):
+        """ Sets current font to font_tuple.
+        
+        Pushes a font tuple (family, style, size) to the font_stack and sets it as 
+        the current font. Make sure to _font_stack_pop() when done.
+        """
+        self.font_stack.append(font_tuple)
+        self.set_font(font_tuple[0], font_tuple[1], font_tuple[2])
+
+    def _font_stack_pop(self):
+        """ Restores the current font to to the previous used one.
+        
+        Pops a font tuple (family, style, size) from the font_stack and sets the  
+        previous font as the  current font. 
+        """
+        if len(self.font_stack) > 0:
+            self.font_stack.pop()
+            last_font = self.font_stack[len(self.font_stack)-1]
+            self.set_font(last_font[0], last_font[1], last_font[2])
+        else:
+            print 'OI!'
+
+    # Overrides
+    def header(self):
+        if self.page_no() == 1:
+            self._print_frontpage_header()
+        else:
+            self._font_stack_push(self.header_font)
+            self.cell(w=self.useable_width/2, txt=self.title, align='L')
+            self.cell(w=self.useable_width/2, txt=self.artist, align='R')
+            self._font_stack_pop()
+
+#== Printing =======================================================================
+
+    def _print_frontpage_header(self):
+        # Song title
+        self._font_stack_push(('Courier', 'U', 20))
+        self.cell(10)
+        self.cell(w=0, txt=self.title, align='L')
+        self._font_stack_pop()
+        self.ln(10)
+        # Artist
+        self._font_stack_push(('Courier', '', 15))
+        self.cell(15)
+        self.cell(w=0, txt=self.artist, align='L')
+        self._font_stack_pop()
+        self.ln(30)
 
     def calculate_width_for_staff(self, chords_string):
         """ Calculates the maximum width for each cell in a row given the string """ 
@@ -45,6 +100,8 @@ class LeadSheet(FPDF):
         if line_break:
             self.ln(self.staff_height)
 
+
+#== Model ==========================================================================
 
     def generate_staff(self, staff_string, staff_width):
         """ Generates a staff containing a defined number of bars.
@@ -97,12 +154,14 @@ def _parse_chord(chord_word):
         return chord_word
 
 
+# tmp driver.
 if __name__ == "__main__":
-    ls = LeadSheet(20, 20, 20)
+    ls = LeadSheet('Macken', 'Galenskaparna')
     ls.add_page()
     ls.print_staff('a F C G')
     ls.print_staff('a F C G')
     ls.print_staff('a F C G')
     ls.print_staff('a F C G')
     ls.print_staff('a F C G')
+    ls.add_page()
     ls.output('test.pdf', 'F')
